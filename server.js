486fb07de897bbff2c3ea601735df3182e0bef4b
Saravanan KR <krascme@gmail.com>
2015-12-08 04:15:03 -0500

    Test Code

---
+++ server.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.get('/', function(req, res) {
+  res.send('success');
+});
+
+
+app.get('/write', function(req, res) {
+
+  fs.writeFile('/var/lib/test/' + process.env.OPENSHIFT_BUILD_NAMESPACE + '.txt', process.env, function(err) {
+    console.log('file written');
+  }); 
+  res.send('write success \n');
+});
+
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
